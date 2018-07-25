4bbb71245687ff2de9ef3331b0f60a116d76cbf7
gearmobile <gearmobile@gmail.com>
2018-07-25 21:15:00 +0300

    check classes in img tag

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,6 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 Vue.use(Vuex);
 import axios from 'axios';
-import xmlToJson from '../utils/xmlToJson.js';
-// import Parser from 'rss-parser';
-// let parser = new Parser();
 
 const state = {
   items: [],
@@ -12,27 +9,32 @@ const state = {
 };
 
 const mutations = {
+  // INIT RSS ITEMS
   INIT_RSS_ITEMS(state, payload) {
     let sample = [...payload];
     sample.forEach(el => {
-      let picture = null;
+      let picture = {};
       const dummy = document.createElement('html');
-      dummy.innerHTML = el.content.trim().slice(0, 200);
+      dummy.innerHTML = el.content.trim().slice(0, 1000);
       const image = dummy.getElementsByTagName('img')['0'];
-      // console.log(dummy.getElementsByTagName('img'));
-      if (image) {
-        picture = {
-          src: image.src,
-          // width: image.width,
-          // height: image.height,
-          className: image.className
-        };
+
+      if (image == null) {
+        picture.className = '';
+        picture.src = '';
+      } else {
+        if (image.className == '') {
+          picture.className = 'b-news-article__center';
+        } else {
+          picture.className = image.className;
+        }
+        picture.src = image.src;
       }
+
       console.log(picture);
     });
-    // console.log(sample);
     state.items = payload;
   },
+  // INIT DATA
   INIT_DATA(state, payload) {
     state.data = payload;
   }
@@ -43,11 +45,6 @@ const actions = {
     axios
       .get('/feed')
       .then(response => {
-        // const xml = new DOMParser().parseFromString(response.data, 'application/xml');
-        // commit('GET_RSS_ITEMS', xmlToJson(xml).rss.channel.item);
-        // console.log(xmlToJson(xml).rss);
-        // console.log(parser.parseString(xml));
-        // console.log(response.data);
         commit('INIT_DATA', response.data);
         commit('INIT_RSS_ITEMS', response.data.items);
       })
