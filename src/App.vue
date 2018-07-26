03e9d1231165aadafed78e7bc95f03dfd20f91dd
gearmobile <gearmobile@gmail.com>
2018-07-26 09:45:14 +0300

    add scroll support

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,92 @@
 <template>
   <div class="rss">
-    <h1 class="rss__title">Новостная лента <span class="uppercase">regru</span></h1>
-    <search />
-    <list />
+    <div class="rss__header rss__header--sticky" :class="sticky">
+      <h1 class="rss__title">Новостная лента <span class="uppercase">regru</span></h1>
+      <search />
+    </div>
+    <div class="rss__content">
+      <list />
+    </div>
   </div>
 </template>
-
 
 <script>
 import Search from './components/Search.vue';
 import List from './components/List.vue';
 export default {
   name: 'appRoot',
+  data() {
+    return {
+      scrolled: false,
+      offset: 5
+    };
+  },
+  methods: {
+    handleScroll() {
+      this.scrolled = window.scrollY > this.offset;
+    }
+  },
+  created() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  destroyed() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
   components: {
     Search,
     List
+  },
+  computed: {
+    sticky() {
+      return this.scrolled ? 'rss__header--scrolled' : '';
+    }
   }
 };
 </script>
 
+<style lang="scss">
 
-<style>
-  .rss {
-    padding: 2rem;
+  body {
+    margin: 0;
+    padding: 0;
   }
+
+  .rss {
+    position: relative;
+
+    &__header {
+      border: 1px solid rgba(0,0,0, .4);
+      padding: .8rem;
+      transition: background-color .2s;
+
+      &--sticky {
+        position: fixed;
+        left: 0;
+        top: 0;
+        // width: 100%;
+        width: 100vw;
+        z-index: 999;
+      }
+
+      &--scrolled {
+        background-color: rgba(0,0,0, .4);
+      }
+    }
+
+    &__content {
+      padding-top: 160px;
+    }
+  }
+
   .uppercase {
     text-transform: uppercase;
   }
+
   .button {
     cursor: pointer;
     text-transform: uppercase;
   }
+
   .button:active,
   .button:focus {
     outline: none;
